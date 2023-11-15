cc21dcf214b66d5b7b3b0eef07d9f2d9d7a6703a
adrianarmm <areyemor@myuax.com>
2023-11-15 22:19:28 +0100

    10

---  Espacio de nombres repartido entre diferentes archivos/triangulo.cpp
+++  Espacio de nombres repartido entre diferentes archivos/triangulo.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 
 namespace Geometria {
+    namespace Triangulo {
+        float area(float base, float altura)
+        {
+            return base * altura / 2;
+        }
+    }
+}
