924ca232f2b22d1f2b96020bf96d88116ceeeb03
adrianarmm <areyemor@myuax.com>
2023-11-15 22:47:00 +0100

    29

--- Declaración de subespacios de nombres/Declaración de subespacios de nombres.cpp
+++ Declaración de subespacios de nombres/Declaración de subespacios de nombres.cpp
@@ -8,12 +8,21 @@ namespace Ciencia {
             return masa * c * c;
         }
 
-double calcularMomentoLineal(double masa, double velocidad) {
+        double calcularMomentoLineal(double masa, double velocidad) {
             return masa * velocidad;
         }
+        double calcularFuerza(double masa, double aceleracion) {
+            return masa * aceleracion;
+        }
+    }
+}
+int main() {
+    std::cout << "La energía de un objeto de 1kg es: " << Ciencia::Fisica::energia(1) << std::endl;
+
+    double masa = 1;
+    double velocidad = 0.5;
+    double momento_lineal = Ciencia::Fisica::calcularMomentoLineal(masa, velocidad);
+    std::cout << "El momento lineal de un objeto de " << masa << "kg y " << velocidad << "m/s es: " << momento_lineal << std::endl;
 
     }
-
-
-
 }
