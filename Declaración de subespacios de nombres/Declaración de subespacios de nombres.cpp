a8cfcbf4628291e51c739055137caa5db191374e
adrianarmm <areyemor@myuax.com>
2023-11-15 22:44:45 +0100

    27

--- Declaración de subespacios de nombres/Declaración de subespacios de nombres.cpp
+++ Declaración de subespacios de nombres/Declaración de subespacios de nombres.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 namespace Ciencia {
+    namespace Fisica {
+        const double c = 299792458;
+
+        double energia(double masa) {
+            return masa * c * c;
+        }
+
+
+    }
+
+
 
 }
