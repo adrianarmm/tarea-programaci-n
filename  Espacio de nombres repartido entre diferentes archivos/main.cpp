9fb222c6b7a5fd53387b893afc6dccd55d8d2bd2
adrianarmm <areyemor@myuax.com>
2023-11-15 22:27:20 +0100

    17

---  Espacio de nombres repartido entre diferentes archivos/main.cpp
+++  Espacio de nombres repartido entre diferentes archivos/main.cpp
@@ -3,3 +3,14 @@
 #include "circulo.cpp"
 
 int main() {
+    double area_triangulo = Geometria::Triangulo::area(5, 3);
+    std::cout << "El área del triángulo es: " << area_triangulo << std::endl;
+    double perimetro_triangulo = Geometria::Triangulo::calcularPerimetro(5, 3, 4);
+    std::cout << "El perímetro del triángulo es: " << perimetro_triangulo << std::endl;
+    double area_circulo = Geometria::Circulo::calcularArea(5);
+    std::cout << "El área del círculo es: " << area_circulo << std::endl;
+    double perimetro_circulo = Geometria::Circulo::calcularPerimetro(5);
+    std::cout << "El perímetro del círculo es: " << perimetro_circulo << std::endl;
+    return 0;
+
+}
