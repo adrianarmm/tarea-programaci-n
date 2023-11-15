c91364a0879611931c90e3d786547160ae7d60b5
adrianarmm <areyemor@myuax.com>
2023-11-15 22:21:56 +0100

    14

---  Espacio de nombres repartido entre diferentes archivos/circulo.cpp
+++  Espacio de nombres repartido entre diferentes archivos/circulo.cpp
@@ -2,5 +2,11 @@
 
 namespace Geometria {
     namespace Circulo {
+        const double PI = 3.14159265358979323846;
+        double calcularArea(double radio)
+        {
+            return PI * radio * radio;
+        }
+    }
 
 }
