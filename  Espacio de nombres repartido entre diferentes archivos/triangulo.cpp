7030dd07f4d6179313de6c6c01d4e1e6b8a97e77
adrianarmm <areyemor@myuax.com>
2023-11-15 22:20:25 +0100

    11

---  Espacio de nombres repartido entre diferentes archivos/triangulo.cpp
+++  Espacio de nombres repartido entre diferentes archivos/triangulo.cpp
@@ -6,5 +6,9 @@ namespace Geometria {
         {
             return base * altura / 2;
         }
+        double calcularPerimetro(double lado1, double lado2, double lado3)
+        {
+            return lado1 + lado2 + lado3;
+        }
     }
 }
