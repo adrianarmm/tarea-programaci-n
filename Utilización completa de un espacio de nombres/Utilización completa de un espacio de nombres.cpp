ff912cdc553a3ae5b7f863d59bd45267c68d893d
adrianarmm <areyemor@myuax.com>
2023-11-15 21:30:10 +0100

    7

--- Utilización completa de un espacio de nombres/Utilización completa de un espacio de nombres.cpp
+++ Utilización completa de un espacio de nombres/Utilización completa de un espacio de nombres.cpp
@@ -11,6 +11,10 @@ namespace Matematicas {
     int multiplicacion(int a, int b) {
         return a * b;
     }
+    float division(float a, float b) {
+        if (b != 0) {
+            return a / b;
+
 }
 int main()
 {
